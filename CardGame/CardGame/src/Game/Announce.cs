8ec9ddb86782071e78649bc123dfffd1267cba8d
Romain Samuel <romain.samuel@epitech.eu>
2017-11-11 01:28:48 +0100

    Unit tests: CardManager and Announces

--- CardGame/CardGame/src/Game/Announce.cs
+++ CardGame/CardGame/src/Game/Announce.cs
@@ -15,6 +15,11 @@
             this.Reward = this.Type.Score();
             this.CardsToValidate = new List<AnnounceCard>();
 
+            if ((int)type.MinimumCardValue() < (int)card.Value)
+            {
+                throw new ArgumentException();
+            }
+
             if (this.Type == AnnounceType.Carre)
             {
                 switch (card.Value)
@@ -28,7 +33,7 @@
                         break;
                 }
 
-                foreach (Card.Types.Type t in Enum.GetValues(typeof(Card.Types.Type)))
+                foreach (Card.Types.Type t in Enum.GetValues(typeof(Card.Types.Type)).OfType<Card.Types.Type>().ToList().Skip(1))
                 {
                     this.CardsToValidate.Add(new AnnounceCard(new Card
                     {
@@ -64,15 +69,18 @@
         {
             foreach (AnnounceCard cardToValidate in this.CardsToValidate)
             {
-                if (cardToValidate.Card.Value == card.Value && cardToValidate.Card.Type == card.Type)
+                if (cardToValidate.Validated == false)
                 {
-                    cardToValidate.Validated = true;
-                    return true;
-                }
+                    if (cardToValidate.Card.Value == card.Value && cardToValidate.Card.Type == card.Type)
+                    {
+                        cardToValidate.Validated = true;
+                        return true;
+                    }
 
-                if (this.Type.OrderMatters())
-                {
-                    return false;
+                    if (this.Type.OrderMatters())
+                    {
+                        return false;
+                    }
                 }
             }
 
@@ -91,7 +99,7 @@
                 return (int)other.Type - (int)this.Type;
             }
 
-            return this.CardsToValidate[0].CompareTo(other.CardsToValidate[0]);
+            return other.CardsToValidate[0].CompareTo(this.CardsToValidate[0]);
         }
 
         public class AnnounceCard
